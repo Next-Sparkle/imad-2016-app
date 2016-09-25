7c5d273d59e396109368b3e216b267efae6eeee3
Next-Sparkle <nsinteractivestudio@gmail.com>
2016-09-25 10:44:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,6 +29,11 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+var counter = 0;
+app.get("/counter" , function (req, res){
+   counter = counter + 1;
+   res.send(counetr.toString());
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
