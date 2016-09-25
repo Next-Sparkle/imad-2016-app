de1840c83ddd3c09060dcd797d5b565763afa96c
Next-Sparkle <nsinteractivestudio@gmail.com>
2016-09-25 19:36:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,12 +4,21 @@ var counter = 0;
 button.onclick = function(){
     
     // Make a request to the counter endpoint
-    
+    var request = new XMLHttpRequest();
     // Capture the response and store it in a variable
-    
-    // Render the variable in the correct span
-
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-}
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE){
+            // Take some action
+            if(request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        // Not yet done
+    };
+        
+        // Make the request
+        request.open('GET', 'http://next-sparkle.imad.hasura-app.io/counter', true);
+        request.send(null);
+};
