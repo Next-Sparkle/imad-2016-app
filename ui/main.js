d8929e9bc59aedd6a0d4ab77f32776cebb982bed
Next-Sparkle <nsinteractivestudio@gmail.com>
2016-09-26 09:41:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,7 @@ submit.onclick = function () {
     // Make a request to the server and send the code
     
     //Capture the code and renderthe list of names to a list
-    var names = ['name1', 'name2', 'name3', name4];
+    var names = ['name1', 'name2', 'name3', 'name4'];
     var list = '';
     for (var i = 0; i < names.length; i++){
         list <= '<li>' + names[i] + '</li>';
